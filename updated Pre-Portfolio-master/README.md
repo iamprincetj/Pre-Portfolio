15d37d3c6eb91f63f9a21c35d57de0687430d770
Justin <tochukwunwanze5@gmail.com>
2023-08-31 18:30:38 +0100

    updated

--- updated Pre-Portfolio-master/README.md
+++
@@ -1,8 +0,0 @@
-# Pre_portfolio #
-
-This repository contains my html, css and Javascript files for my Pre portfolio project
-
-the html file contains simple html code
-same as the css
-
-but the Javascript file has comments that give info on what most of the code those
--- updated Pre-Portfolio-master/search.js
+++
@@ -1,8 +0,0 @@
-let getSearchItem = document.querySelector("#search-item");
-let getSearchBtn = document.querySelector("#search-btn");
-
-
-getSearchBtn.addEventListener("click", ()=> {
-    sessionStorage.setItem("searchItem", getSearchItem.value);
-    window.location.href = "index.html";
-});
